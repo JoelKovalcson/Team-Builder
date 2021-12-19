33ab521a2ff17ef2b2ce07b1da01ca32b9cba112
Joel Kovalcson <joelkovalcson@gmail.com>
2021-12-19 11:18:55 -0800

    Finished html generation

--- utils/buildTeam.js
+++ utils/buildTeam.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const jsdom = require("jsdom");
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
 const { JSDOM } = jsdom;
 
 function buildTeam(employees) {
-    
+
     // Copy the stylesheet
     fs.copyFileSync('./src/styles.css', './dist/styles.css');
     // Read HTML template
@@ -20,11 +23,26 @@ function buildTeam(employees) {
         let div = $("<div>");
         let name = $("<h2>");
         let role = $("<h3>");
+        let list = $("<ul>");
+        let id = $("<li>");
+        let email = $("<li>");
+        let special = $("<li>");
 
         name.html(employee.getName());
         role.html(employee.getRole());
+        id.html(employee.getID());
+        email.html(employee.getEmail());
+        if (employee instanceof Manager) special.html(employee.getOfficeNumber());
+        else if (employee instanceof Intern) special.html(employee.getSchool());
+        else if (employee instanceof Engineer) special.html(employee.getGithub());
+
+        list.append(id);
+        list.append(email);
+        list.append(special);
 
         div.append(name);
+        div.append(role);
+        div.append(list);
         cardSection.append(div);
     });
 
